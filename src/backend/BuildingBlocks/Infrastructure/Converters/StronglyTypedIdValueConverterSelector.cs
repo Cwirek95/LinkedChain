b1183b09b38aa5cc03005f57b36621b987f28e96
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-03-21 12:19:11 +0100

    feat(buildingBlocks): add Converters to Infra
    Create TypedIdValueConverter and StronglyTypedIdValueConverterSelector
    classes for Entity Framework Core

---
+++ src/backend/BuildingBlocks/Infrastructure/Converters/StronglyTypedIdValueConverterSelector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+using LinkedChain.BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace LinkedChain.BuildingBlocks.Infrastructure.Converters;
+
+public class StronglyTypedIdValueConverterSelector : ValueConverterSelector
+{
+    private readonly ConcurrentDictionary<(Type ModelClrType, Type ProviderClrType), ValueConverterInfo> _converters
+        = new ConcurrentDictionary<(Type ModelClrType, Type ProviderClrType), ValueConverterInfo>();
+
+    public StronglyTypedIdValueConverterSelector(ValueConverterSelectorDependencies dependencies)
+        : base(dependencies)
+    {
+    }
+
+    public override IEnumerable<ValueConverterInfo> Select(Type modelClrType, Type providerClrType = null)
+    {
+        var baseConverters = base.Select(modelClrType, providerClrType);
+        foreach (var converter in baseConverters)
+        {
+            yield return converter;
+        }
+
+        var underlyingModelType = UnwrapNullableType(modelClrType);
+        var underlyingProviderType = UnwrapNullableType(providerClrType);
+
+        if (underlyingProviderType is null || underlyingProviderType == typeof(Guid))
+        {
+            var isTypedIdValue = typeof(TypedIdValueBase).IsAssignableFrom(underlyingModelType);
+            if (isTypedIdValue)
+            {
+                var converterType = typeof(TypedIdValueConverter<>).MakeGenericType(underlyingModelType);
+
+                yield return _converters.GetOrAdd((underlyingModelType, typeof(Guid)), _ =>
+                {
+                    return new ValueConverterInfo(
+                        modelClrType: modelClrType,
+                        providerClrType: typeof(Guid),
+                        factory: valueConverterInfo => (ValueConverter)Activator.CreateInstance(converterType, valueConverterInfo.MappingHints));
+                });
+            }
+        }
+    }
+
+    private static Type? UnwrapNullableType(Type? type)
+        => type is null ? null : Nullable.GetUnderlyingType(type) ?? type;
+}
---
+++ src/backend/BuildingBlocks/Infrastructure/Converters/TypedIdValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using LinkedChain.BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace LinkedChain.BuildingBlocks.Infrastructure.Converters;
+
+public class TypedIdValueConverter<TTypedIdValue> : ValueConverter<TTypedIdValue, Guid>
+    where TTypedIdValue : TypedIdValueBase
+{
+    public TypedIdValueConverter(ConverterMappingHints mappingHints = null)
+        : base(id => id.Value,
+            value => Create(value), mappingHints)
+    {
+    }
+
+    private static TTypedIdValue Create(Guid id) 
+        => Activator.CreateInstance(typeof(TTypedIdValue), id) as TTypedIdValue;
+}
