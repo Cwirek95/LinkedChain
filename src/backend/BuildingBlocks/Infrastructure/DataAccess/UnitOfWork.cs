0b1828859e799ffc7d420c73e3ef247d71f37fe6
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-03-21 16:57:50 +0100

    feat(buildingBlocks): Decorate UnitOfWork
    Add UnitOfWorkCommandHandlerDecorator class and call DispatchEventsAsync
    in UnitOfWork

--- src/backend/BuildingBlocks/Infrastructure/DataAccess/UnitOfWork.cs
+++ src/backend/BuildingBlocks/Infrastructure/DataAccess/UnitOfWork.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LinkedChain.BuildingBlocks.Infrastructure.DomainEvents;
+using Microsoft.EntityFrameworkCore;
 
 namespace LinkedChain.BuildingBlocks.Infrastructure.DataAccess;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly DbContext _context;
+    private readonly IDomainEventsDispatcher _domainEventsDispatcher;
 
-    public UnitOfWork(DbContext context)
+    public UnitOfWork(DbContext context, IDomainEventsDispatcher domainEventsDispatcher)
     {
         _context = context;
+        _domainEventsDispatcher = domainEventsDispatcher;
     }
 
     public async Task<int> CommitAsync(CancellationToken cancellationToken = default, Guid? internalCommandId = null)
     {
+        await _domainEventsDispatcher.DispatchEventsAsync();
+        
         return await _context.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ src/backend/BuildingBlocks/Infrastructure/DataAccess/UnitOfWorkCommandHandlerDecorator.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace LinkedChain.BuildingBlocks.Infrastructure.DataAccess;
+
+public class UnitOfWorkCommandHandlerDecorator<T> : IRequestHandler<T>
+    where T : IRequest
+{
+    private readonly IRequestHandler<T> _decorated;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UnitOfWorkCommandHandlerDecorator(
+        IRequestHandler<T> decorated,
+        IUnitOfWork unitOfWork)
+    {
+        _decorated = decorated;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(T command, CancellationToken cancellationToken)
+    {
+        await _decorated.Handle(command, cancellationToken);
+        await _unitOfWork.CommitAsync(cancellationToken);
+    }
+}
