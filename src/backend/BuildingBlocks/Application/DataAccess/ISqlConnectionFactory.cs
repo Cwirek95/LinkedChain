2d593fa612eae3ceb8c3da6686e26a52012657d6
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-03-22 10:17:43 +0100

    feat(buildingBlocks): add SqlConnectionFactory

---
+++ src/backend/BuildingBlocks/Application/DataAccess/ISqlConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace LinkedChain.BuildingBlocks.Application.DataAccess;
+
+public interface ISqlConnectionFactory
+{
+    IDbConnection GetOpenConnection();
+
+    IDbConnection CreateNewConnection();
+
+    string GetConnectionString();
+}
---
+++ src/backend/BuildingBlocks/Infrastructure/DataAccess/SqlConnectionFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using LinkedChain.BuildingBlocks.Application.DataAccess;
+
+namespace LinkedChain.BuildingBlocks.Infrastructure.DataAccess;
+
+public class SqlConnectionFactory : ISqlConnectionFactory, IDisposable
+{
+    private readonly string _connectionString;
+    private IDbConnection? _connection;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection GetOpenConnection()
+    {
+        if (_connection?.State == ConnectionState.Open)
+            return _connection;
+        
+        _connection = new SqlConnection(_connectionString);
+        _connection.Open();
+
+        return _connection;
+    }
+
+    public IDbConnection CreateNewConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        connection.Open();
+
+        return connection;
+    }
+
+    public string GetConnectionString()
+    {
+        return _connectionString;
+    }
+
+    public void Dispose()
+    {
+        if (_connection is { State: ConnectionState.Open })
+        {
+            _connection.Dispose();
+        }
+    }
+}
