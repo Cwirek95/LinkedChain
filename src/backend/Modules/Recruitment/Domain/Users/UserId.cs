0baaf4488ac22d56b6e3a0ef65985ecff605c6c5
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-04-04 18:12:37 +0200

    feat(recruitment): add UserId to Domain

---
+++ src/backend/Modules/Recruitment/Domain/Users/UserId.cs
@@ -0,0 +1,11 @@
+﻿using LinkedChain.BuildingBlocks.Domain;
+
+namespace LinkedChain.Modules.Recruitment.Domain.Users;
+
+public class UserId : TypedIdValueBase
+{
+    public UserId(Guid value)
+        : base(value)
+    {
+    }
+}
