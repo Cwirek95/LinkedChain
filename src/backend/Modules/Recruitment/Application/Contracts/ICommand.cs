39d5e4eebef7e0d2727680ba19e7a520406e97fb
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-05-28 20:22:04 +0200

    feat: add query and command base class/interfaces

---
+++ src/backend/Modules/Recruitment/Application/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace LinkedChain.Modules.Recruitment.Application.Contracts;
+
+public interface ICommand<out TResult> : IRequest<TResult>
+{
+    Guid Id { get; }
+}
+
+public interface ICommand : IRequest
+{
+    Guid Id { get; }
+}
---
+++ src/backend/Modules/Recruitment/Application/Contracts/IQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace LinkedChain.Modules.Recruitment.Application.Contracts;
+
+public interface IQuery<out TResult> : IRequest<TResult>
+{
+}
