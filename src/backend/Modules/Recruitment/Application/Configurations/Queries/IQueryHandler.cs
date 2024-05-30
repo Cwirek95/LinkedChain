4e585027b9c8ae19bdd7f63bebf834fe28fce211
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-05-30 15:19:03 +0200

    feat: add offer query handler

---
+++ src/backend/Modules/Recruitment/Application/Configurations/Queries/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using LinkedChain.Modules.Recruitment.Application.Contracts;
+using MediatR;
+
+namespace LinkedChain.Modules.Recruitment.Application.Configurations.Queries;
+
+public interface IQueryHandler<in TQuery, TResult> :
+    IRequestHandler<TQuery, TResult>
+    where TQuery : IQuery<TResult>
+{
+}
