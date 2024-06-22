917aed2c220257048c6e18e04835b57a94c7f7d9
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-06-22 20:57:01 +0200

    feat: create recruitment infr events module

---
+++ src/backend/Modules/Recruitment/Infrastructure/Configuration/EventsBus/EventsBusModule.cs
@@ -0,0 +1,20 @@
+﻿using LinkedChain.BuildingBlocks.Infrastructure.EventBus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkedChain.Modules.Recruitment.Infrastructure.Configuration.EventsBus;
+
+public static class EventsBusModule
+{
+    public static IServiceCollection AddEventsBusModule(this IServiceCollection services, IEventsBus eventsBus = null)
+    {
+        if (eventsBus is null)
+        {
+            services.AddSingleton(eventsBus);
+        }
+        else
+        {
+            services.AddSingleton<IEventsBus, InMemoryEventBusClient>();
+        }
+        return services;
+    }
+}
---
+++ src/backend/Modules/Recruitment/Infrastructure/Configuration/EventsBus/IntegrationEventGenericHandler.cs
@@ -0,0 +1,39 @@
+﻿using LinkedChain.BuildingBlocks.Application.DataAccess;
+using LinkedChain.BuildingBlocks.Infrastructure.EventBus;
+using LinkedChain.BuildingBlocks.Infrastructure.Serializations;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Dapper;
+
+namespace LinkedChain.Modules.Recruitment.Infrastructure.Configuration.EventsBus;
+
+internal class IntegrationEventGenericHandler<T> : IIntegrationEventHandler<T>
+    where T : IntegrationEvent
+{
+    public async Task Handle(T @event)
+    {
+        using (var scope = RecruitmentsCompositionRoot.BeginLifetimeScope())
+        {
+            var sqlConnectionFactory = scope.ServiceProvider.GetRequiredService<ISqlConnectionFactory>();
+            using (var connection = sqlConnectionFactory.GetOpenConnection())
+            {
+                string type = @event.GetType().FullName;
+                var data = JsonConvert.SerializeObject(@event, new JsonSerializerSettings
+                {
+                    ContractResolver = new AllPropertiesContractResolver()
+                });
+
+                var sql = "INSERT INTO [recruitments].[InboxMessages] (Id, OccurredOn, Type, Data) " +
+                          "VALUES (@Id, @OccurredOn, @Type, @Data)";
+
+                await connection.ExecuteScalarAsync(sql, new
+                {
+                    @event.Id,
+                    @event.OccurredOn,
+                    type,
+                    data
+                });
+            }
+        }
+    }
+}
