609647fed30985cce25b628201da9e2d1a2108df
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-06-23 17:42:42 +0200

    feat: create logging decorator to recruitment infr

---
+++ src/backend/Modules/Recruitment/Infrastructure/Configuration/Processing/CommandsExecutor.cs
@@ -0,0 +1,26 @@
+﻿using LinkedChain.Modules.Recruitment.Application.Contracts;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkedChain.Modules.Recruitment.Infrastructure.Configuration.Processing;
+
+internal static class CommandsExecutor
+{
+    internal static async Task Execute(ICommand command)
+    {
+        using (var scope = RecruitmentsCompositionRoot.BeginLifetimeScope())
+        {
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            await mediator.Send(command);
+        }
+    }
+
+    internal static async Task<TResult> Execute<TResult>(ICommand<TResult> command)
+    {
+        using (var scope = RecruitmentsCompositionRoot.BeginLifetimeScope())
+        {
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            return await mediator.Send(command);
+        }
+    }
+}
---
+++ src/backend/Modules/Recruitment/Infrastructure/Configuration/Processing/IRecurringCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace LinkedChain.Modules.Recruitment.Infrastructure.Configuration.Processing;
+
+public interface IRecurringCommand
+{
+}
