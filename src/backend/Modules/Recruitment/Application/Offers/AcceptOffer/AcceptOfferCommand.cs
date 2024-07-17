8cbacc0ecc8af614c25426a0cd858bfd80a3c802
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-07-17 16:09:03 +0200

    feat: add accept offer command

---
+++ src/backend/Modules/Recruitment/Application/Offers/AcceptOffer/AcceptOfferCommand.cs
@@ -0,0 +1,13 @@
+﻿using LinkedChain.Modules.Recruitment.Application.Contracts;
+
+namespace LinkedChain.Modules.Recruitment.Application.Offers.AcceptOffer;
+
+public sealed class AcceptOfferCommand : CommandBase
+{
+    public AcceptOfferCommand(Guid offerId)
+    {
+        OfferId = offerId;
+    }
+
+    public Guid OfferId { get; }
+}
---
+++ src/backend/Modules/Recruitment/Application/Offers/AcceptOffer/AcceptOfferCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using LinkedChain.Modules.Recruitment.Application.Configurations.Commands;
+using LinkedChain.Modules.Recruitment.Domain.Offer;
+
+namespace LinkedChain.Modules.Recruitment.Application.Offers.AcceptOffer;
+
+internal class AcceptOfferCommandHandler : ICommandHandler<AcceptOfferCommand>
+{
+    private readonly IOfferRepository _offerRepository;
+
+    public AcceptOfferCommandHandler(IOfferRepository offerRepository)
+    {
+        _offerRepository = offerRepository;
+    }
+
+    public async Task Handle(AcceptOfferCommand request, CancellationToken cancellationToken)
+    {
+        var offer = await _offerRepository.GetByIdAsync(new OfferId(request.OfferId));
+
+        offer.Reject();
+    }
+}
