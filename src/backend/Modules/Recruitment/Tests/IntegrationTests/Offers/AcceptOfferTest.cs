2d0facf37a259610d8df6d1a8967354441c6a6ff
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-09-04 12:12:43 +0200

    test: add AcceptOfferTest

---
+++ src/backend/Modules/Recruitment/Tests/IntegrationTests/Offers/AcceptOfferTest.cs
@@ -0,0 +1,34 @@
+﻿using LinkedChain.Modules.Recruitment.Application.Offers.AcceptOffer;
+using LinkedChain.Modules.Recruitment.Application.Offers.CreatePermanentContractOffer;
+using LinkedChain.Modules.Recruitment.Application.Offers.GetOfferDetails;
+using LinkedChain.Modules.Recruitment.Domain.Offer;
+using LinkedChain.Modules.Recruitment.IntegrationTests.SeedWork;
+using NUnit.Framework;
+
+namespace LinkedChain.Modules.Recruitment.IntegrationTests.Offers;
+
+[TestFixture]
+public class AcceptOfferTest : TestBase
+{
+    public async Task AcceptOffer_Test()
+    {
+        // Act
+        var offerId = await RecruitmentModule.ExecuteCommandAsync(new CreatePermanentContractOfferCommand(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            "Test description",
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddDays(30),
+            PayPeriod.Hourly.ToString(),
+            "PLN",
+            30.00M));
+
+        await RecruitmentModule.ExecuteCommandAsync(new AcceptOfferCommand(offerId));
+
+        var offerDetails = await RecruitmentModule.ExecuteQueryAsync(new GetOfferDetailsQuery(offerId));
+
+        // Assert
+        Assert.That(offerDetails, Is.Not.Null);
+        Assert.Equals(offerDetails.StatusName, OfferStatus.Accepted.ToString());
+    }
+}
