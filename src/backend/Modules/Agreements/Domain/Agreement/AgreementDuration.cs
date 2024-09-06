0eea8109853eade5b1753351e22e50fa5aa2d8c1
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-09-06 15:40:57 +0200

    feat: init Agreements and add AgreementDuration

---
+++ src/backend/Modules/Agreements/Domain/Agreement/AgreementDuration.cs
@@ -0,0 +1,20 @@
+﻿using LinkedChain.BuildingBlocks.Domain;
+
+namespace LinkedChain.Modules.Agreements.Domain.Agreement;
+
+public class AgreementDuration : ValueObject
+{
+    public DateTime StartDate { get; }
+    public DateTime? EndDate { get; }
+
+    private AgreementDuration(DateTime startDate, DateTime? endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    public static AgreementDuration CreateNewBetweenDates(DateTime startDate, DateTime? endDate)
+    { 
+        return new AgreementDuration(startDate, endDate);
+    }
+}
