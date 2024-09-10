05e72b8354526d48cf76f2e854def6f984d34b3a
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-09-10 16:27:29 +0200

    feat: create basic B2BAgreement domain entity

---
+++ src/backend/Modules/Agreements/Domain/Agreement/OfferId.cs
@@ -0,0 +1,11 @@
+﻿using LinkedChain.BuildingBlocks.Domain;
+
+namespace LinkedChain.Modules.Agreements.Domain.Agreement;
+
+public class OfferId : TypedIdValueBase
+{
+    public OfferId(Guid value)
+        : base(value)
+    {
+    }
+}
