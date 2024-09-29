d4f206df31a6583161fb775d95cc89f7c4ba523d
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-09-29 16:47:02 +0200

    tests: add Agreements domain test helpers

---
+++ src/backend/Modules/Agreements/Tests/UnitTests/SeedWork/TestBase.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using LinkedChain.BuildingBlocks.Domain;
+using LinkedChain.BuildingBlocks.Domain.Events;
+using LinkedChain.BuildingBlocks.Domain.Exceptions;
+using LinkedChain.Modules.Agreements.Domain.SharedKernel;
+using NUnit.Framework;
+
+namespace LinkedChain.Modules.Agreements.Domain.UnitTests.SeedWork;
+
+public abstract class TestBase
+{
+    public static T AssertPublishedDomainEvent<T>(Entity aggregate)
+        where T : IDomainEvent
+    {
+        var domainEvent = DomainEventsTestHelper.GetAllDomainEvents(aggregate).OfType<T>().SingleOrDefault();
+
+        if (domainEvent == null)
+        {
+            throw new Exception($"{typeof(T).Name} event not published");
+        }
+
+        return domainEvent;
+    }
+
+    public static void AssertDomainEventNotPublished<T>(Entity aggregate)
+        where T : IDomainEvent
+    {
+        var domainEvent = DomainEventsTestHelper.GetAllDomainEvents(aggregate).OfType<T>().SingleOrDefault();
+        Assert.That(domainEvent, Is.Null);
+    }
+
+    public static List<T> AssertPublishedDomainEvents<T>(Entity aggregate)
+        where T : IDomainEvent
+    {
+        var domainEvents = DomainEventsTestHelper.GetAllDomainEvents(aggregate).OfType<T>().ToList();
+
+        if (!domainEvents.Any())
+        {
+            throw new Exception($"{typeof(T).Name} event not published");
+        }
+
+        return domainEvents;
+    }
+
+    public static void AssertBrokenRule<TRule>(TestDelegate testDelegate)
+        where TRule : class, IBusinessRule
+    {
+        var message = $"Expected {typeof(TRule).Name} broken rule";
+        var businessRuleValidationException = Assert.Catch<BusinessRuleValidationException>(testDelegate, message);
+        if (businessRuleValidationException != null)
+        {
+            businessRuleValidationException.BrokenRule.Should().BeOfType<TRule>();
+        }
+    }
+
+    public static void AssertBrokenRule<TRule>(AsyncTestDelegate testDelegate)
+        where TRule : class, IBusinessRule
+    {
+        var message = $"Expected {typeof(TRule).Name} broken rule";
+        var businessRuleValidationException = Assert.CatchAsync<BusinessRuleValidationException>(testDelegate, message);
+        if (businessRuleValidationException != null)
+        {
+            Assert.That(businessRuleValidationException.BrokenRule, Is.TypeOf<TRule>(), message);
+        }
+    }
+
+    [TearDown]
+    public void AfterEachTest()
+    {
+        SystemClock.Reset();
+    }
+}
