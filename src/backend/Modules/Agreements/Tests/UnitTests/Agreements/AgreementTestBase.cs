b1342a4cb9e766a702407d17452632dce74e3eda
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-10-01 20:03:13 +0200

    tests: add AgreementTestBase

---
+++ src/backend/Modules/Agreements/Tests/UnitTests/Agreements/AgreementTestBase.cs
@@ -0,0 +1,42 @@
+﻿using LinkedChain.Modules.Agreements.Domain.Agreement;
+using LinkedChain.Modules.Agreements.Domain.SharedKernel;
+using LinkedChain.Modules.Agreements.Domain.UnitTests.SeedWork;
+using LinkedChain.Modules.Agreements.Domain.Users;
+
+namespace LinkedChain.Modules.Agreements.Domain.UnitTests.Agreements;
+
+public class AgreementTestBase : TestBase
+{
+    protected PernamentContractAgreement CreatePermanentContractAgreementTestData()
+    {
+        return PernamentContractAgreement.CreateNew(
+            new OfferId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            "Description",
+            AgreementDuration.Create(SystemClock.Now, SystemClock.Now.AddMonths(12)),
+            Salary.Of(5000, "PLN", PayPeriod.Monthly));
+    }
+
+    protected MandateContractAgreement CreateMandateContractAgreementTestData()
+    {
+        return MandateContractAgreement.CreateNew(
+            new OfferId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            "Description",
+            AgreementDuration.Create(SystemClock.Now, SystemClock.Now.AddMonths(1)),
+            Salary.Of(50, "PLN", PayPeriod.Hourly));
+    }
+
+    protected B2BAgreement CreateB2BAgreementTestData()
+    {
+        return B2BAgreement.CreateNew(
+            new OfferId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            new UserId(Guid.NewGuid()),
+            "Description",
+            AgreementDuration.Create(SystemClock.Now, SystemClock.Now.AddMonths(6)),
+            Salary.Of(350, "PLN", PayPeriod.Daily));
+    }
+}
