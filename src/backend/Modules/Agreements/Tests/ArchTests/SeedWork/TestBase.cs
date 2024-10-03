2cf35efe60f8087eecf6f1b3cfc3cab2e3eddb99
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-10-03 15:10:49 +0200

    tests: init Agreements ArchTests and TestBase

---
+++ src/backend/Modules/Agreements/Tests/ArchTests/SeedWork/TestBase.cs
@@ -0,0 +1,36 @@
+﻿using LinkedChain.Modules.Agreements.Domain.Agreement;
+using NetArchTest.Rules;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace LinkedChain.Modules.Agreements.Domain.ArchTests.SeedWork;
+
+public abstract class TestBase
+{
+    protected static Assembly DomainAssembly => typeof(B2BAgreement).Assembly;
+
+    protected static void AssertAreImmutable(IEnumerable<Type> types)
+    {
+        List<Type> failingTypes = new();
+        foreach (var type in types)
+        {
+            if (type.GetFields().Any(x => !x.IsInitOnly) || type.GetProperties().Any(x => x.CanWrite))
+            {
+                failingTypes.Add(type);
+                break;
+            }
+        }
+
+        AssertFailingTypes(failingTypes);
+    }
+
+    protected static void AssertFailingTypes(IEnumerable<Type> types)
+    {
+        Assert.That(types, Is.Null.Or.Empty);
+    }
+
+    protected static void AssertArchTestResult(TestResult result)
+    {
+        AssertFailingTypes(result.FailingTypes);
+    }
+}
