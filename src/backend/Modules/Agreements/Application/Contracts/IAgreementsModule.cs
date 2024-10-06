55571f0e84be0ac90ae8cc9ef64099e988df6fbc
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-10-06 18:36:57 +0200

    feat: add Agreements Application Contracts

---
+++ src/backend/Modules/Agreements/Application/Contracts/IAgreementsModule.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedChain.Modules.Agreements.Application.Contracts;
+
+public interface IAgreementsModule
+{
+    Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+
+    Task ExecuteCommandAsync(ICommand command);
+
+    Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+}
---
+++ src/backend/Modules/Agreements/Application/Contracts/IQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace LinkedChain.Modules.Agreements.Application.Contracts;
+
+public interface IQuery<out TResult> : IRequest<TResult>
+{
+}
