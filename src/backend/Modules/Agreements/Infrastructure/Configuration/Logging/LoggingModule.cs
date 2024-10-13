5ddad5cd6ab67ae43afcebb00b4827ba119ca72d
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-10-13 12:04:12 +0200

    feat: add loggin and mediator module to Agreements

---
+++ src/backend/Modules/Agreements/Infrastructure/Configuration/Logging/LoggingModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace LinkedChain.Modules.Agreements.Infrastructure.Configuration.Logging;
+
+public static class LoggingModule
+{
+    public static IServiceCollection AddLoggingModule(this IServiceCollection services, ILogger logger)
+    {
+        services.AddSingleton(logger);
+
+        return services;
+    }
+}
