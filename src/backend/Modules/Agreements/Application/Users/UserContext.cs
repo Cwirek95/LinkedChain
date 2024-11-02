9975f9947f733ad861702411e287fddc7f400712
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-11-02 21:00:09 +0100

    feat: add agreements authentication module

---
+++ src/backend/Modules/Agreements/Application/Users/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using LinkedChain.BuildingBlocks.Application;
+using LinkedChain.Modules.Agreements.Domain.Users;
+
+namespace LinkedChain.Modules.Agreements.Application.Users;
+
+public class UserContext : IUserContext
+{
+    private readonly IExecutionContextAccessor _executionContextAccessor;
+
+    public UserContext(IExecutionContextAccessor executionContextAccessor)
+    {
+        _executionContextAccessor = executionContextAccessor;
+    }
+
+    public UserId UserId => new UserId(_executionContextAccessor.UserId);
+}
---
+++ src/backend/Modules/Agreements/Infrastructure/Configuration/Authenctication/AuthencticationModule.cs
@@ -0,0 +1,15 @@
+﻿using LinkedChain.Modules.Agreements.Application.Users;
+using LinkedChain.Modules.Agreements.Domain.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkedChain.Modules.Agreements.Infrastructure.Configuration.Authenctication;
+
+public static class AuthenticationModule
+{
+    public static IServiceCollection AddAuthenticationModule(this IServiceCollection services)
+    {
+        services.AddScoped<IUserContext, UserContext>();
+
+        return services;
+    }
+}
