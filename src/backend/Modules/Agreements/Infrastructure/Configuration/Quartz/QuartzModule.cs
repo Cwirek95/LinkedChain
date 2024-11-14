b84445b8843a86a6df6d71c2be490b9a6b181827
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-11-14 17:43:55 +0100

    feat: add quartz to Agreements infra

---
+++ src/backend/Modules/Agreements/Infrastructure/Configuration/Quartz/QuartzModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System.Reflection;
+
+namespace LinkedChain.Modules.Agreements.Infrastructure.Configuration.Quartz;
+
+public static class QuartzModule
+{
+    public static IServiceCollection AddQuartzModule(this IServiceCollection services, Assembly assembly)
+    {
+        var jobTypes = assembly.GetTypes().Where(x => typeof(IJob).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+
+        foreach (var jobType in jobTypes)
+        {
+            services.AddTransient(typeof(IJob), jobType);
+        }
+
+        return services;
+    }
+}
