bc2f1fe92cfc6af7b0f1687c26ea5b0ec3b5201d
Andrzej Ćwiertniak <acwiertniak95@gmail.com>
2024-11-15 17:42:41 +0100

    feat: create agreements startup and module

---
+++ src/backend/Modules/Agreements/Infrastructure/AgreementsModule.cs
@@ -0,0 +1,30 @@
+﻿using LinkedChain.Modules.Agreements.Application.Contracts;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.Processing;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkedChain.Modules.Agreements.Infrastructure;
+
+public class AgreementsModule : IAgreementsModule
+{
+    public async Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command)
+    {
+        return await CommandsExecutor.Execute(command);
+    }
+
+    public async Task ExecuteCommandAsync(ICommand command)
+    {
+        await CommandsExecutor.Execute(command);
+    }
+
+    public async Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query)
+    {
+        using (var scope = AgreementsCompositionRoot.BeginLifetimeScope())
+        {
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+            return await mediator.Send(query);
+        }
+    }
+}
---
+++ src/backend/Modules/Agreements/Infrastructure/Configuration/AgreementsStartup.cs
@@ -0,0 +1,72 @@
+﻿using LinkedChain.BuildingBlocks.Application;
+using LinkedChain.BuildingBlocks.Infrastructure.Common;
+using LinkedChain.BuildingBlocks.Infrastructure.EventBus;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.Authenctication;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.DataAccess;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.EventsBus;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.Mediator;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.Outbox;
+using LinkedChain.Modules.Agreements.Infrastructure.Configuration.Quartz;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System.Reflection;
+
+namespace LinkedChain.Modules.Agreements.Infrastructure.Configuration;
+
+public class AgreementsStartup
+{
+    public static void Initialize(
+        IServiceCollection services,
+        string connectionString,
+        IExecutionContextAccessor executionContextAccessor,
+        ILogger logger,
+        IEventsBus eventsBus,
+        long? internalProcessingPoolingInterval = null)
+    {
+        var moduleLogger = logger.ForContext("Module", "Agreements");
+
+        ConfigureCompositionRoot(
+            services,
+            connectionString,
+            executionContextAccessor,
+            moduleLogger,
+            eventsBus);
+
+        QuartzStartup.Initialize(moduleLogger, internalProcessingPoolingInterval);
+
+        EventsBusStartup.Initialize(moduleLogger);
+    }
+
+    public static void Stop()
+    {
+        QuartzStartup.StopQuartz();
+    }
+
+    private static void ConfigureCompositionRoot(
+        IServiceCollection services,
+        string connectionString,
+        IExecutionContextAccessor executionContextAccessor,
+        ILogger logger,
+        IEventsBus eventsBus)
+    {
+        var assemblies = new[] { typeof(IMediator).Assembly, Assembly.GetExecutingAssembly() };
+        var assembly = typeof(IJob).Assembly;
+
+        services.AddSingleton(logger.ForContext("Module", "Agreements"));
+
+        var loggerFactory = new SerilogLoggerFactory(logger);
+        services.AddDataAccessModule(connectionString, loggerFactory);
+        services.AddEventsBusModule(eventsBus);
+        services.AddMediationModule(assemblies);
+        services.AddAuthenticationModule();
+
+        var domainNotificationsMap = new BiDictionary<string, Type>();
+        services.AddOutboxModule(domainNotificationsMap);
+        services.AddQuartzModule(assembly);
+
+        services.AddSingleton(executionContextAccessor);
+    }
+}
